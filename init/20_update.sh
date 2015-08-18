9c4348fe0b54f8465fb516be0f6d741803f9a7cf
sparklyballs <sparklyballs@gmail.com>
2015-08-18 11:24:53 +0100

    add warning panel for update script

--- init/20_update.sh
+++ init/20_update.sh
@@ -1,28 +1,48 @@
 #!/bin/bash
-export DEBIAN_FRONTEND=noninteractive
+
+# clean up any potential files in /tmp that may interfere with this script
+if [ -f "/tmp/LATEST" ]; then
+rm /tmp/LATEST
+fi
+if [ -f "/tmp/"*.deb ]; then
+rm /tmp/*.deb
+fi
+if [ -f "/tmp/warn.nfo" ]; then
+rm /tmp/warn.nfo
+fi
+
+# set what to display if we are going to upgrade/downgrade main version
+WARN_SET='/tmp/warn.nfo'
+cat > $WARN_SET <<-WARNSIGN
+##########################################################################
+# You have set the version variable to a different main version than is  #
+# locally installed, this script will downgrade/upgrade to the version   #
+# you have chosen, make sure you have set the variable correctly and all #
+# your local kodi installs are at the same main version if you want to   #
+# share libraries with them.                                             #
+##########################################################################
+WARNSIGN
+
+
+# check what version we currently have installed
 INSTALLED=`dpkg-query -W -f='${Version}' kodi-headless`
 
 
 FETCH_VER=${INSTALLED%.*}
 
 
-if [ -f "/tmp/LATEST" ]; then
-rm /tmp/LATEST
-fi
-
+# get file containing latest build of our chosen main version
 wget -nd -nH -O /tmp/LATEST https://raw.githubusercontent.com/linuxserver/misc-files/master/kodi/LATEST$FETCH_VER
 LATEST=$(cat /tmp/LATEST)
 VERSION=$(sed 's/.*kodi-headless//' /tmp/LATEST | sed 's/amd64.deb//g' | sed 's/_//g')
 
+# decide if we need to update local version, if not exit from script gracefully
 if [ "$VERSION" == "$INSTALLED" ]; then
 echo "No Update Required"
 exit 0;
 fi
 
-if [ -f "/tmp/*.deb" ]; then
-rm /tmp/*.deb
-fi
-
+# install latest build of our chosen main version
 apt-get remove --purge -y kodi-headless
 wget -nd -nH -O /tmp/kodi-headless.deb  https://github.com/linuxserver/misc-files/blob/master/kodi/$LATEST?raw=true
 apt-get update -qq
