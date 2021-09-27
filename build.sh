c2b420e56f18ed6b487bebee638bf255d8e29e16
Matt Huisman <gitlab@matthuisman.nz>
2021-09-27 14:54:17 +1300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker buildx create --use --name builder
 docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag matthuisman/kodi-headless:Matrix .
