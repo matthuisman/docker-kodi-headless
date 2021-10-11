b47180f2c04dc76509809f259fd118590372d869
Matt Huisman <gitlab@matthuisman.nz>
2021-10-11 16:21:54 +1300

    Update build and docs

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
+docker login
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker buildx create --name multiarch --driver docker-container --use
-docker buildx build --push --platform linux/arm/v7,linux/amd64 --tag matthuisman/kodi-headless:Matrix --pull --no-cache .
+docker buildx build --push --platform linux/amd64 --tag matthuisman/kodi-headless:Matrix-amd64 --pull .
+docker buildx build --push --platform linux/arm64/v8 --tag matthuisman/kodi-headless:Matrix-arm64 --pull .
+docker buildx build --push --platform linux/arm/v6 --tag matthuisman/kodi-headless:Matrix-arm --pull -f Dockerfile.arm .
+docker manifest create matthuisman/kodi-headless:Matrix matthuisman/kodi-headless:Matrix-arm matthuisman/kodi-headless:Matrix-amd64 matthuisman/kodi-headless:Matrix-arm64
+docker manifest push --purge matthuisman/kodi-headless:Matrix
